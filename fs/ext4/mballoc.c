f880a158e1929c1070a4a7859f5c6bea3ba39ad5
Kemeng Shi <shikemeng@huaweicloud.com>
2024-01-05 17:20:54 +0800

    ext4: remove unused return value of __mb_check_buddy
    [ Upstream commit 133de5a0d8f8e32b34feaa8beae7a189482f1856 ]
    
    Remove unused return value of __mb_check_buddy.
    
    Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20240105092102.496631-2-shikemeng@huaweicloud.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Stable-dep-of: d9ee3ff810f1 ("ext4: improve integrity checking in __mb_check_buddy by enhancing order-0 validation")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 14c5a790b4a0cd533bfe1e7109bf2dbec781378d)
    
    Conflicts:
    	fs/ext4/mballoc.c
    Notes: ignore context change.
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -541,7 +541,7 @@ do {									\
 	}								\
 } while (0)
 
-static int __mb_check_buddy(struct ext4_buddy *e4b, char *file,
+static void __mb_check_buddy(struct ext4_buddy *e4b, char *file,
 				const char *function, int line)
 {
 	struct super_block *sb = e4b->bd_sb;
@@ -562,7 +562,7 @@ static int __mb_check_buddy(struct ext4_buddy *e4b, char *file,
 	{
 		static int mb_check_counter;
 		if (mb_check_counter++ % 100 != 0)
-			return 0;
+			return;
 	}
 
 	while (order > 1) {
@@ -627,6 +627,8 @@ static int __mb_check_buddy(struct ext4_buddy *e4b, char *file,
 	MB_CHECK_ASSERT(e4b->bd_info->bb_fragments == fragments);
 
 	grp = ext4_get_group_info(sb, e4b->bd_group);
+	if (!grp)
+		return;
 	list_for_each(cur, &grp->bb_prealloc_list) {
 		ext4_group_t groupnr;
 		struct ext4_prealloc_space *pa;
@@ -636,7 +638,6 @@ static int __mb_check_buddy(struct ext4_buddy *e4b, char *file,
 		for (i = 0; i < pa->pa_len; i++)
 			MB_CHECK_ASSERT(mb_test_bit(k + i, buddy));
 	}
-	return 0;
 }
 #undef MB_CHECK_ASSERT
 #define mb_check_buddy(e4b) __mb_check_buddy(e4b,	\
